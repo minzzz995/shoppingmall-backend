44c38d023eb5cd825c4fcb4d7a9533ee126ea943
minzzz995 <minzzz995@gmail.com>
2024-10-25 11:20:32 +0900

    cors

--- app.js
+++ app.js
@@ -1,23 +1,28 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const indexRouter = require("./routes/index")
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const indexRouter = require("./routes/index");
+const app = express();
 
-require("dotenv").config()
-app.use(cors())
-app.use(bodyParser.urlencoded({extended:false}))
-app.use(bodyParser.json())
+require("dotenv").config();
 
-app.use("/api", indexRouter)
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://shoppingmall-front.netlify.app'],
+  credentials: true,
+}));
 
-const mongoURI = process.env.LOVAL_DB_ADDRESS
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.use("/api", indexRouter);
+
+const mongoURI = process.env.LOVAL_DB_ADDRESS;
 mongoose
     .connect(mongoURI, {useNewUrlParser:true})
     .then(() => console.log("mongoose connected"))
-    .catch((err) => console.log("DB Connection fail", err))
+    .catch((err) => console.log("DB Connection fail", err));
 
 app.listen(process.env.PORT || 5000, ()=>{
-    console.log("server on")
-})
+    console.log("server on");
+});
