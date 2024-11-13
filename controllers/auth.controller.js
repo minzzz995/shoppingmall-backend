fa4167bf73f12890a7f8cf56a7a324c730a63521
jmj <jmj@iwaz.co.kr>
2024-11-13 18:25:53 +0900

    backend-server

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,6 +4,8 @@ const bcrypt = require("bcryptjs")
 const authController = {}
 require("dotenv").config();
 const JWT_SECRET_KEY=process.env.JWT_SECRET_KEY
+const {GoogleAuth, OAuth2Client} = require('google-auth-library');
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID
 
 authController.loginWithEmail = async (req, res) => {
     try {
@@ -16,6 +18,24 @@ authController.loginWithEmail = async (req, res) => {
                 return res.status(200).json({status:"success", user, token})
             }
         }
+        throw new Error("invalid email or password")
+    } catch(error){
+        res.status(400).json({status:"fail", error: error.message})
+    }
+}
+
+authController.loginWithGoogle = async (req, res) => {
+    try {
+        const { token } = req.body
+        console.log("Received token:", token);
+        const googleClient = new OAuth2Client(GOOGLE_CLIENT_ID)
+        const ticket = await googleClient.verifyIdToken({
+            idToken:token,
+            audience:GOOGLE_CLIENT_ID
+        })
+
+        const { email, name } = ticket.getPayload()
+        console.log(email, name)
         throw new Error("invalid email or password")
     } catch(error){
         res.status(400).json({status:"fail", error: error.message})
